f9bdab010edd92295540db9861a67b3914ae9ef7
Harpreet Janday <54457483+hjanday@users.noreply.github.com>
2021-07-29 11:21:25 -0400

    Update notionify.py

--- notionify.py
+++ notionify.py
@@ -1,3 +1,4 @@
+#Import all the utilities needed
 import discord
 from discord.ext import commands
 from datetime import date,time, datetime
@@ -6,6 +7,8 @@ from notion.client import NotionClient
 from notion.block import TodoBlock
 from notion.block import TextBlock
 from notion.block import ToggleBlock
+
+#Define Timestamps to log added data
 now = datetime.now()
 currentDay = date.today()
 currDate = currentDay.strftime("%B %d, %Y")
@@ -20,9 +23,9 @@ n_token = open("notion.txt", "r").read()
 d_token = open("TOKEN.txt", "r").read()
 n_client = NotionClient(token_v2=n_token)
 
-chosen_page = n_client.get_block("link")
+chosen_page = n_client.get_block("link_to_page")
 
-
+#On startup command
 @bot_client.event
 async def bootup():
     print('Bot is currently online')
